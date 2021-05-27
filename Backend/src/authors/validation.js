0ff675dd6e7a5da748e39391ade0d3f67d334abd
Arvand Molla <mr.arvand@gmail.com>
2021-05-27 19:08:05 +0200

    BE removed

--- Backend/src/authors/validation.js
+++
@@ -1,8 +0,0 @@
-import { body } from "express-validator";
-
-export const authorsValidator = [
-  body("name").exists().withMessage("name is requiered"),
-  body("surname").exists().withMessage("surname is requiered"),
-  body("email").exists().withMessage("email is requiered"),
-  body("dateOfBirth").exists().withMessage("Birth date is requiered"),
-];
--- Backend/src/posts/validation.js
+++
@@ -1,7 +0,0 @@
-import { body } from "express-validator";
-
-export const postsValidator = [
-  body("title").exists().withMessage("title is requiered"),
-  body("category").exists().withMessage("category is requiered"),
-  body("content").exists().withMessage("content is requiered"),
-];
